6b71f7501fa68effb9133d6f26ed0392d796b635
zakovyrya <zakovyrya@27776ab6-20da-11df-b9f8-bfe9128881a6>
2010-02-24 01:24:38 +0000

    Cleanup

--- Tabi/current/iPhone/Classes/ScreenSaverController.m
+++ Tabi/current/iPhone/Classes/ScreenSaverController.m
@@ -6,6 +6,7 @@
 //  Copyright 2010 __MyCompanyName__. All rights reserved.
 //
 
+
 #import "ScreenSaverController.h"
 
 
@@ -21,28 +22,10 @@
     [super viewWillDisappear:animated];
 }
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.wantsFullScreenLayout = YES;
 }
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
 
 - (void)didReceiveMemoryWarning {
 	// Releases the view if it doesn't have a superview.
